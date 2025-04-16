b22e864527cb0d6f922d55e411a397ea8dcc9ecd
sanskar-ck <sjam@ciklum.com>
2025-04-16 13:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def predict_api():
 
 if __name__ == '__main__':
     # Explicitly specify the host and port
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8000)
